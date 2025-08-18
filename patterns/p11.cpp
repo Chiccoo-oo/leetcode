b0ece1c565e154bd13d7426d1b4ca60f895d0a74
Anushka Ekka <anushkaekka23@gmail.com>
2025-08-18 11:33:56 +0530

    679

--- patterns/p11.cpp
+++ patterns/p11.cpp
@@ -5,17 +5,14 @@ void p11(int n)
 {
     for (int i = 1; i <= n; i++)
     {
-        // Determine the starting number for the current row.
-        // If the row number (i) is odd, the starting number is 1.
-        // If the row number (i) is even, the starting number is 0.
+
         int start = (i % 2 == 1) ? 1 : 0;
 
         for (int j = 1; j <= i; j++)
         {
-            // Print the number and a space.
+
             cout << start << " ";
 
-            // Toggle the number for the next iteration.
             start = 1 - start;
         }
         cout << endl;
