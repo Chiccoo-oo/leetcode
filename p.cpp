e65b0d2f6d17e0454c476a66feb1011c5405cfa3
Anushka Ekka <anushkaekka23@gmail.com>
2025-09-02 11:34:38 +0530

    3025

---
+++ p.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 11;
+    int sum = 0;
+    if (x <= 10)
+    {
+        sum = sum + 10;
+        }
+    else
+    {
+        cout << "no";
+    }
+    cout << sum;
+}
