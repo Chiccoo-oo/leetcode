b6790fb4f7c681ea6d09ddbac50f8f91a69a063f
Anushka Ekka <anushkaekka23@gmail.com>
2026-02-03 09:38:59 +0530

    3637

---
+++ 35.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int l = 0;
+        int n = nums.size();
+        int r = n - 1;
+        while (l <= r)
+        {
+            int m = (l + r) / 2;
+            if (nums[m] == target)
+            {
+                return m;
+            }
+            else if (nums[m] > target)
+            {
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+};
